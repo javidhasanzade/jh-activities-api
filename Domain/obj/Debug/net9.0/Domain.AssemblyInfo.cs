03c139a2653cad490a27341852ba49a78059ee04
Javid Hasanzade <J.Hasanzade@fp-dbs.com>
2025-11-11 13:15:23 +0100

    ef core, a new migration

--- Domain/obj/Debug/net9.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net9.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2425d1e7c01857649d500ec04ddd1d30ac1d2d6b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92648a0270773353d42036cd85718b04ba8d154b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
