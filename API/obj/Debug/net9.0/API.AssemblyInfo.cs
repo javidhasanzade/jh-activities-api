c2c5fe70f31e6b327912934b9ba2841bbfb7f28b
Javid Hasanzade <jh.grunger@gmail.com>
2025-11-14 14:29:02 +0100

    upgraded the version to dotnet 10

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92648a0270773353d42036cd85718b04ba8d154b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66fc79204293658936cb794bc46ac388e1847ea9")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
