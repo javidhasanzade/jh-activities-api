28b19d3a575b94c72cc47f17d2c326ee467fff43
javidhasanzade <jh.grunger@gmail.com>
2025-11-25 05:44:48 +0100

    added new query get activity details, refactored controllers a bit

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,28 +1,20 @@
 ﻿using Application.Activities.Queries;
 using Domain;
-using MediatR;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using Persistence;
 
 namespace API.Controllers;
 
-public class ActivitiesController(AppDbContext dbContext, IMediator mediator) : BaseApiController
+public class ActivitiesController : BaseApiController
 {
     [HttpGet]
     public async Task<ActionResult<List<Activity>>> GetActivities()
     {
-        return await mediator.Send(new GetActivityList.Query());
+        return await Mediator.Send(new GetActivityList.Query());
     }
 
     [HttpGet("{id}")]
     public async Task<ActionResult<Activity>> GetActivity(string id)
     {
-        var activity = await dbContext.Activities.FindAsync(id);
-
-        if (activity == null)
-            return NotFound();
-        
-        return activity;
+        return await Mediator.Send(new GetActivityDetails.Query { Id = id });
     }
 }
