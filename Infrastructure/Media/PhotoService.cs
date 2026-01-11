5783392cb40b41e3eb3e8b764ef3bd13c767a1eb
javidhasanzade <jh.grunger@gmail.com>
2026-01-11 23:26:41 +0100

    implemented photo service using cloudinary

--- Infrastructure/Media/PhotoService.cs
+++ Infrastructure/Media/PhotoService.cs
@@ -1,18 +1,54 @@
 ﻿using Application.Interfaces;
 using Application.Profiles.DTOs;
+using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Options;
 
 namespace Infrastructure.Media;
 
 public class PhotoService : IPhotoService
 {
-    public Task<PhotoUploadResult> UploadPhoto(IFormFile file)
+    private readonly Cloudinary _cloudinary;
+
+    public PhotoService(IOptions<CloudinarySettings> cloudinaryOptions)
     {
-        throw new NotImplementedException();
+        var account = new Account(cloudinaryOptions.Value.CloudName, cloudinaryOptions.Value.ApiKey,
+            cloudinaryOptions.Value.ApiSecret);
+
+        _cloudinary = new Cloudinary(account);
     }
 
-    public Task<string> DeletePhoto(string publicId)
+    public async Task<PhotoUploadResult?> UploadPhoto(IFormFile file)
     {
-        throw new NotImplementedException();
+        if (file.Length <= 0) return null;
+
+        await using var stream = file.OpenReadStream();
+
+        var uploadParams = new ImageUploadParams
+        {
+            File = new FileDescription(file.FileName, stream),
+            Transformation = new Transformation().Height(500).Width(500).Crop("fill"),
+            Folder = "JH-Activities-API"
+        };
+
+        var uploadResult = await _cloudinary.UploadAsync(uploadParams);
+
+        if (uploadResult.Error != null) throw new Exception(uploadResult.Error.Message);
+
+        return new PhotoUploadResult
+        {
+            PublicId = uploadResult.PublicId,
+            Url = uploadResult.Url.AbsoluteUri
+        };
+    }
+
+    public async Task<string> DeletePhoto(string publicId)
+    {
+        var deleteParams = new DeletionParams(publicId);
+
+        var result = await _cloudinary.DestroyAsync(deleteParams);
+
+        return result.Error != null ? throw new Exception(result.Error.Message) : result.Result;
     }
 }
