2fbf0f19a5dc199068e9a4f5fa8d73f0d42b40d2
javidhasanzade <jh.grunger@gmail.com>
2026-01-11 23:17:32 +0100

    Added photo service and registered it

--- API/Program.cs
+++ API/Program.cs
@@ -38,6 +38,7 @@ builder.Services.AddMediatR(opt =>
     }
 );
 builder.Services.AddScoped<IUserAccessor, UserAccessor>();
+builder.Services.AddScoped<IPhotoService, PhotoService>();
 builder.Services.AddAutoMapper(typeof(MappingProfiles).Assembly);
 builder.Services.AddValidatorsFromAssemblyContaining<CreateActivityValidator>();
 builder.Services.AddTransient<ExceptionMiddleware>();
---
+++ Infrastructure/Media/PhotoService.cs
@@ -0,0 +1,18 @@
+﻿using Application.Interfaces;
+using Application.Profiles.DTOs;
+using Microsoft.AspNetCore.Http;
+
+namespace Infrastructure.Media;
+
+public class PhotoService : IPhotoService
+{
+    public Task<PhotoUploadResult> UploadPhoto(IFormFile file)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<string> DeletePhoto(string publicId)
+    {
+        throw new NotImplementedException();
+    }
+}
