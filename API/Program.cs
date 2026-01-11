bb693c0ee2f7024c5cc294658d03dbfbf5c425db
javidhasanzade <jh.grunger@gmail.com>
2026-01-11 23:09:49 +0100

    addded settings for cloudinary, installed nuget package for it

--- API/Program.cs
+++ API/Program.cs
@@ -5,6 +5,7 @@ using Application.Core;
 using Application.Interfaces;
 using Domain;
 using FluentValidation;
+using Infrastructure.Media;
 using Infrastructure.Security;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
@@ -51,6 +52,7 @@ builder.Services.AddAuthorization(opt =>
     });
 });
 builder.Services.AddTransient<IAuthorizationHandler, IsHostRequirementHandler>();
+builder.Services.Configure<CloudinarySettings>(builder.Configuration.GetSection("CloudinarySettings"));
 
 var app = builder.Build();
 
---
+++ Infrastructure/Media/CloudinarySettings.cs
@@ -0,0 +1,8 @@
+﻿namespace Infrastructure.Media;
+
+public class CloudinarySettings
+{
+    public required string CloudName { get; set; }
+    public required string ApiKey { get; set; }
+    public required string ApiSecret { get; set; }
+}
