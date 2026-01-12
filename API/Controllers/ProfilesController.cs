1c4ee0afb20614c2a258c29ecc65a208581391d8
javidhasanzade <jh.grunger@gmail.com>
2026-01-12 23:43:25 +0100

    new endpoint and query for getting all photos of user

--- API/Controllers/ProfilesController.cs
+++ API/Controllers/ProfilesController.cs
@@ -1,4 +1,5 @@
 ﻿using Application.Profiles.Commands;
+using Application.Profiles.Queries;
 using Domain;
 using Microsoft.AspNetCore.Mvc;
 
@@ -10,5 +11,11 @@ public class ProfilesController : BaseApiController
     public async Task<ActionResult<Photo>> AddPhoto(IFormFile file)
     {
         return HandleResult(await Mediator.Send(new AddPhoto.Command { File = file }));
+    }
+
+    [HttpGet("{userId}/photos")]
+    public async Task<ActionResult<List<Photo>>> GetPhotosForUser(string userId)
+    {
+        return HandleResult(await Mediator.Send(new GetProfilePhotos.Query { UserId = userId }));
     }
 }
