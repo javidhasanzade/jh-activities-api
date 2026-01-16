46d5a9939a5e80d2a74f9ddca2f77539a75e6197
javidhasanzade <jh.grunger@gmail.com>
2026-01-16 20:02:37 +0100

    Added comment entity with relationships, migration

--- Domain/Activity.cs
+++ Domain/Activity.cs
@@ -8,13 +8,14 @@ public class Activity
     public required string Description { get; set; }
     public required string Category { get; set; }
     public bool IsCancelled { get; set; }
-    
+
     // location
     public required string City { get; set; }
     public required string Venue { get; set; }
     public double Latitude { get; set; }
     public double Longitude { get; set; }
-   
+
     // navigation properties
     public ICollection<ActivityAttendee> Attendees { get; set; } = [];
+    public ICollection<Comment> Comments { get; set; } = [];
 }
