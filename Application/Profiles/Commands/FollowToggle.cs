4eef0c0855a38c2bab7b1e3b1c398535d3d4911c
javidhasanzade <jh.grunger@gmail.com>
2026-01-18 18:55:42 +0100

    follow toggle command and an endpoint for it

---
+++ Application/Profiles/Commands/FollowToggle.cs
@@ -0,0 +1,40 @@
+﻿using Application.Core;
+using Application.Interfaces;
+using Domain;
+using MediatR;
+using Persistence;
+
+namespace Application.Profiles.Commands;
+
+public class FollowToggle
+{
+    public class Command : IRequest<Result<Unit>>
+    {
+        public required string TargetUserId { get; set; }
+    }
+
+    public class Handler(AppDbContext dbContext, IUserAccessor userAccessor) : IRequestHandler<Command, Result<Unit>>
+    {
+        public async Task<Result<Unit>> Handle(Command request, CancellationToken cancellationToken)
+        {
+            var observer = await userAccessor.GetUserAsync();
+            var target = await dbContext.Users.FindAsync([request.TargetUserId], cancellationToken);
+
+            if (target == null) return Result<Unit>.Failure("Target user not found", 400);
+
+            var following = await dbContext.UserFollowings.FindAsync([observer.Id, target.Id], cancellationToken);
+
+            if (following == null)
+                dbContext.UserFollowings.Add(new UserFollowing
+                {
+                    ObserverId = observer.Id,
+                    TargetId = target.Id
+                });
+            else dbContext.UserFollowings.Remove(following);
+
+            return await dbContext.SaveChangesAsync(cancellationToken) > 0
+                ? Result<Unit>.Success(Unit.Value)
+                : Result<Unit>.Failure("Failed to follow user", 400);
+        }
+    }
+}
