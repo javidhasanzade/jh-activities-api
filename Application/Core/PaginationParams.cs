b755b67a8c514d4c5c74c9365e733cba04ec7af3
javidhasanzade <jh.grunger@gmail.com>
2026-01-28 22:47:10 +0100

    added filters and refactored get activity list query handler

---
+++ Application/Core/PaginationParams.cs
@@ -0,0 +1,13 @@
+﻿namespace Application.Core;
+
+public class PaginationParams<TCursor>
+{
+    private const int MaxPageSize = 50;
+    public TCursor? Cursor { get; set; }
+
+    public int PageSize
+    {
+        get;
+        set => field = (value > MaxPageSize) ? MaxPageSize : value;
+    }
+}
